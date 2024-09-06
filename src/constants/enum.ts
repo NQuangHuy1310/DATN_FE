bc830c0d998f935baad899ffc47ff1ce37359248
HuyNQ <kaisa13102004@gmail.com>
2024-09-06 20:39:01 +0700

    feature/config-message

--- src/constants/enum.ts
+++ src/constants/enum.ts
@@ -1,3 +1,18 @@
-export enum RequestStatusEnum {}
+/* eslint-disable no-unused-vars */
+export enum ApiStatusCode {
+    Success = 200 /** Yêu cầu thành công */,
 
-export enum ResultCode {}
+    Created = 201 /** Tạo dữ liệu thành công */,
+
+    NoContent = 204 /** Không có nội dung để trả về */,
+
+    BadRequest = 400 /** Yêu cầu không hợp lệ */,
+
+    Unauthorized = 401 /** Người dùng chưa được xác thực */,
+
+    NotFound = 404 /** Tài nguyên không tìm thấy */,
+
+    InternalServerError = 500 /** Lỗi máy chủ */,
+
+    GatewayTimeout = 504 /** Thời gian chờ của máy chủ đã hết */
+}
