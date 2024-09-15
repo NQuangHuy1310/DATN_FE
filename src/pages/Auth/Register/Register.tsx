ddb0f163967d516de66fe9fde3e76151c1a38df7
Le Dinh Dung <dev.dungld@gmail.com>
2024-09-15 07:45:02 +0700

    refactor/login-register

--- src/pages/Auth/Register/Register.tsx
+++ src/pages/Auth/Register/Register.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+
 import { FcGoogle } from 'react-icons/fc'
 import { FaFacebook } from 'react-icons/fa'
 import { IoEyeOffSharp, IoEyeSharp } from 'react-icons/io5'
 
 import config from '~/config'
-import { Button } from '~/components/ui/button/Button'
 import Input from '~/components/ui/input/Input'
+import { Button } from '~/components/ui/button/Button'
 
 const Register = () => {
     const [showPassword, setShowPassword] = useState(false)
@@ -15,115 +16,78 @@ const Register = () => {
         setShowPassword(!showPassword)
     }
     return (
-        <div className="relative h-screen">
-            <div className="w-full max-w-[456px] h-auto p-6 sm:p-8 md:p-10 lg:p-12 rounded-2xl shadow absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white ">
-                <div>
-                    <h1 className="font-bold text-DSM text-center">Đăng ký</h1>
-                    <p className="my-5 text-center text-gray-400">
-                        Bạn đã có tài khoản?
-                        <Link className="text-callouts-background-primary" to={config.routes.login}>
-                            {' '}
-                            Đăng nhập
-                        </Link>
-                    </p>
-                </div>
-                <div className="flex gap-3 justify-center mb-3">
-                    <div className="w-20 h-10 sm:w-24 sm:h-11 rounded-xl py-2 px-6 border cursor-pointer bg-white hover:bg-opacity-60">
-                        <FaFacebook className="w-6 h-6 mx-auto text-callouts-background-primary" />
+        <div className="max-w-[550px] mx-auto rounded-xl mt-32 border shadow p-10">
+            <div>
+                <h1 className='text-TXL font-medium mb-1'>Đăng ký để tiếp tục</h1>
+                <p className='text-gray-500'>Sử dụng tài khoản email hoặc dịch vụ khác để đăng ký</p>
+            </div>
+            <div className="mt-5 text-center">
+                <form action="" className="max-w-[400px] mx-auto">
+                    <div className="w-full mb-4">
+                        <Input type="text" placeholder="Họ và tên" name="fullName" id="fullName" className="w-full mt-2" />
                     </div>
-                    <div className="w-20 h-10 sm:w-24 sm:h-11 rounded-xl py-2 px-6 border cursor-pointer bg-white hover:bg-opacity-60">
-                        <FcGoogle className="w-6 h-6 mx-auto" />
+                    <div className="w-full mb-4">
+                        <Input type="text" placeholder="Email" name="email" id="email" className="w-full mt-2" />
                     </div>
-                </div>
-                <div className="flex items-center justify-center my-6">
-                    <div className="w-10 border-t border-gray-300 flex-grow ms-6 me-2"></div>
-                    <span className="text-gray-400">hoặc đăng nhập email</span>
-                    <div className="w-10 border-t border-gray-300 flex-grow ms-2 me-6"></div>
-                </div>
-                <div className="flex items-center justify-center">
-                    <form action="" className="w-full max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto">
-                        <div className="w-full mb-4">
-                            <label htmlFor="fullName" className="font-medium text-TMD">
-                                Họ và tên
-                            </label>
-                            <Input
-                                type="text"
-                                placeholder="Họ và tên"
-                                name="fullName"
-                                id="fullName"
-                                className="w-full mt-2"
+                    <div className="w-full relative mb-4">
+                        <Input
+                            type={showPassword ? 'text' : 'password'} // Thay đổi type dựa trên state
+                            name="password"
+                            id="password"
+                            placeholder="Mật khẩu"
+                            className="w-full mt-2"
+                        />
+                        {showPassword ? (
+                            <IoEyeOffSharp
+                                onClick={togglePasswordVisibility}
+                                className="absolute right-3 top-1/4 transform translate-y-1/2 cursor-pointer text-gray-500"
                             />
+                        ) : (
+                            <IoEyeSharp
+                                onClick={togglePasswordVisibility}
+                                className="absolute right-3 top-1/4 transform translate-y-1/2 cursor-pointer text-gray-500"
+                            />
+                        )}
+                    </div>
+                    <div className="w-full relative mb-4">
+                        <Input
+                            type={showPassword ? 'text' : 'password'} // Thay đổi type dựa trên state
+                            name="confirm_password"
+                            id="confirm_password"
+                            placeholder="Xác nhận mật khẩu"
+                            className="w-full mt-2"
+                        />
+                        {showPassword ? (
+                            <IoEyeOffSharp
+                                onClick={togglePasswordVisibility}
+                                className="absolute right-3 top-1/4 transform translate-y-1/2 cursor-pointer text-gray-500"
+                            />
+                        ) : (
+                            <IoEyeSharp
+                                onClick={togglePasswordVisibility}
+                                className="absolute right-3 top-1/4 transform translate-y-1/2 cursor-pointer text-gray-500"
+                            />
+                        )}
+                    </div>
+
+                    <div>
+                        <Button className="w-full">Đăng ký</Button>
+                    </div>
+                    <div className='mt-5 flex flex-row justify-center gap-5 md:gap-0 md:flex-col '>
+                        <div className='flex items-center gap-10 border-[1px] outline-none border-neutral-stroke-primary rounded-md px-lg py-md h-[40px] text-TMD text-black font-normal focus:border-interactive-stroke-primaryFocus'>
+                            <FaFacebook className='text-callouts-background-primary size-6 ' />
+                            <span className='font-medium hidden md:block'>Đăng nhập với Facebook</span>
+                        </div>
+                        <div className='flex items-center gap-10 border-[1px] outline-none border-neutral-stroke-primary rounded-md px-lg py-md h-[40px] text-TMD text-black font-normal focus:border-interactive-stroke-primaryFocus md:mt-3'>
+                            <FcGoogle className='text-callouts-background-primary size-6' />
+                            <span className='font-medium hidden md:block'>Đăng nhập với Google</span>
                         </div>
 
-                        <div className="w-full mb-4">
-                            <label htmlFor="email" className="font-medium text-TMD">
-                                Email
-                            </label>
-                            <Input type="text" placeholder="Email" name="email" id="email" className="w-full mt-2" />
-                        </div>
-                        <div className="w-full relative mb-4">
-                            <label htmlFor="password" className="font-medium text-TMD">
-                                Mật khẩu
-                            </label>
-                            <Input
-                                type={showPassword ? 'text' : 'password'} // Thay đổi type dựa trên state
-                                name="password"
-                                id="password"
-                                placeholder="Mật khẩu"
-                                className="w-full mt-2"
-                            />
-                            {showPassword ? (
-                                <IoEyeOffSharp
-                                    onClick={togglePasswordVisibility}
-                                    className="absolute right-3 top-1/2 transform translate-y-1/2 cursor-pointer text-gray-500"
-                                />
-                            ) : (
-                                <IoEyeSharp
-                                    onClick={togglePasswordVisibility}
-                                    className="absolute right-3 top-1/2 transform translate-y-1/2 cursor-pointer text-gray-500"
-                                />
-                            )}
-                        </div>
-                        <div className="w-full relative mb-4">
-                            <label htmlFor="password" className="font-medium text-TMD">
-                                Xác nhận mật khẩu
-                            </label>
-                            <Input
-                                type={showPassword ? 'text' : 'password'} // Thay đổi type dựa trên state
-                                name="password"
-                                id="password"
-                                placeholder="Xác nhận mật khẩu"
-                                className="w-full mt-2"
-                            />
-                            {showPassword ? (
-                                <IoEyeOffSharp
-                                    onClick={togglePasswordVisibility}
-                                    className="absolute right-3 top-1/2 transform translate-y-1/2 cursor-pointer text-gray-500"
-                                />
-                            ) : (
-                                <IoEyeSharp
-                                    onClick={togglePasswordVisibility}
-                                    className="absolute right-3 top-1/2 transform translate-y-1/2 cursor-pointer text-gray-500"
-                                />
-                            )}
-                        </div>
-
-                        <div className="flex items-center mb-5">
-                            <Input
-                                type="checkbox"
-                                className=" w-3 h-3  bg-gray-100 border-gray-300 rounded focus:ring-1 focus:ring-blue-500"
-                                id="agree"
-                            />
-                            <label htmlFor="agree" className="ml-2 text-TSM text-gray-400 cursor-pointer">
-                                Tôi đồng ý với điều khoản
-                            </label>
-                        </div>
-
-                        <div>
-                            <Button className="w-full">Đăng ký</Button>
-                        </div>
-                    </form>
-                </div>
+                    </div>
+                    <div className='text-start mt-5'>
+                        <p className='text-gray-500'>Bạn đã có tài khoản? <Link to={config.routes.login} className='text-callouts-background-primary'>Đăng nhập</Link></p>
+                    </div>
+                </form>
             </div>
         </div>
     )
